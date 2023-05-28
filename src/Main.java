945952742cf675ec3a11889c1e0e4a640e3a69aa
ArkTalos <nathanvallad99@gmail.com>
2023-05-28 17:56:34 -0600

    fixed the issue with it not connecting to git/

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@
 // then press Enter. You can now see whitespace characters in your code.
 
 // add package here
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // get string from user
         System.out.println("this program will count the number of vowels in a string");
         System.out.println("Enter a text: ");
-        String userIn = System.console().readLine();
+        Scanner userInput = new Scanner(System.in);
+        String userString = userInput.nextLine();
         // runs the loop that itterares through the string and finds vowels
+        int vowelCount = 0;
+        for (int i = 0; i < userString.length(); i++) {
+            char vowel = userString.charAt(i);
+            if (vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u') {
+                vowelCount++;
+            }
+        }
     }
 }
