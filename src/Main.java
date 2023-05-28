fa1bd770ed658868be5445ce0ee9e1849db1d0ee
ArkTalos <nathanvallad99@gmail.com>
2023-05-28 12:25:17 -0600

    create Git repository.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    import java.io.*;
+    public static void main(String[] args) {
+        // get string from user
+        System.out.println("Enter a text: ");
+        String userIn = System.console().readLine();
+        // runs the loop that itterares through the string and finds vowels
+    }
+}
+}
