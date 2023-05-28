10aea9366cda966210375b996c70fa74919e538e
ArkTalos <nathanvallad99@gmail.com>
2023-05-28 12:32:21 -0600

    fixed import isstue with get_interface

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+// add package here
+import java.io.*;
+
 public class Main {
-    import java.io.*;
     public static void main(String[] args) {
         // get string from user
         System.out.println("this program will count the number of vowels in a string");
